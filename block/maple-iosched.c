083a45f70632489920889bf50c65294e1b8c6672
chhapil <ru2raj@gmail.com>
2018-02-01 08:58:33 +0530

    compilation fixes

--- block/maple-iosched.c
+++ block/maple-iosched.c
@@ -17,20 +17,20 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/slab.h>
-#include <linux/state_notifier.h>
+#include <linux/display_state.h>
 
 #define MAPLE_IOSCHED_PATCHLEVEL	(8)
 
 enum { ASYNC, SYNC };
 
 /* Tunables */
-static const int sync_read_expire = 350;	/* max time before a read sync is submitted. */
-static const int sync_write_expire = 550;	/* max time before a write sync is submitted. */
-static const int async_read_expire = 250;	/* ditto for read async, these limits are SOFT! */
-static const int async_write_expire = 450;	/* ditto for write async, these limits are SOFT! */
+static const int sync_read_expire = 100;	/* max time before a read sync is submitted. */
+static const int sync_write_expire = 350;	/* max time before a write sync is submitted. */
+static const int async_read_expire = 200;	/* ditto for read async, these limits are SOFT! */
+static const int async_write_expire = 500;	/* ditto for write async, these limits are SOFT! */
 static const int fifo_batch = 16;		/* # of sequential requests treated as one by the above parameters. */
-static const int writes_starved = 4;		/* max times reads can starve a write */
-static const int sleep_latency_multiple = 10;	/* multple for expire time when device is asleep */
+static const int writes_starved = 3;		/* max times reads can starve a write */
+static const int sleep_latency_multiple = 5;	/* multple for expire time when device is asleep */
 
 /* Elevator data */
 struct maple_data {
@@ -78,6 +78,7 @@ maple_add_request(struct request_queue *q, struct request *rq)
 	struct maple_data *mdata = maple_get_data(q);
 	const int sync = rq_is_sync(rq);
 	const int dir = rq_data_dir(rq);
+	const bool display_on = is_display_on();
 
 	/*
 	 * Add request to the proper fifo list and set its
@@ -86,11 +87,11 @@ maple_add_request(struct request_queue *q, struct request *rq)
 
    	/* inrease expiration when device is asleep */
    	unsigned int fifo_expire_suspended = mdata->fifo_expire[sync][dir] * sleep_latency_multiple;
-   	if (!state_suspended && mdata->fifo_expire[sync][dir]) {
+   	if (display_on && mdata->fifo_expire[sync][dir]) {
+   		rq->fifo_time = jiffies + mdata->fifo_expire[sync][dir];
+   		list_add_tail(&rq->queuelist, &mdata->fifo_list[sync][dir]);
+   	} else if (!display_on && fifo_expire_suspended) {
 		rq->fifo_time = jiffies + mdata->fifo_expire[sync][dir];
-		list_add_tail(&rq->queuelist, &mdata->fifo_list[sync][dir]);
-   	} else if (state_suspended && fifo_expire_suspended) {
-		rq->fifo_time = jiffies + fifo_expire_suspended;
    		list_add_tail(&rq->queuelist, &mdata->fifo_list[sync][dir]);
    	}
 }
@@ -108,7 +109,7 @@ maple_expired_request(struct maple_data *mdata, int sync, int data_dir)
 	rq = rq_entry_fifo(list->next);
 
 	/* Request has expired */
-    if (time_after_eq(jiffies, rq->fifo_time))
+        if (time_after(jiffies, rq->fifo_time))
 		return rq;
 
 	return NULL;
@@ -205,6 +206,7 @@ maple_dispatch_requests(struct request_queue *q, int force)
 	struct maple_data *mdata = maple_get_data(q);
 	struct request *rq = NULL;
 	int data_dir = READ;
+	const bool display_on = is_display_on();
 
 	/*
 	 * Retrieve any expired request after a batch of
@@ -216,9 +218,9 @@ maple_dispatch_requests(struct request_queue *q, int force)
 	/* Retrieve request */
 	if (!rq) {
 		/* Treat writes fairly while suspended, otherwise allow them to be starved */
-		if (!state_suspended && mdata->starved >= mdata->writes_starved)
+		if (display_on && mdata->starved >= mdata->writes_starved)
 			data_dir = WRITE;
-		else if (state_suspended && mdata->starved >= 1)
+		else if (!display_on && mdata->starved >= 1)
 			data_dir = WRITE;
 
 		rq = maple_choose_request(mdata, data_dir);
